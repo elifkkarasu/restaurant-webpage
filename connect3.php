ea121683216c6894a109098c31983f0082df2014
Elif Karasu <113525121+elifkkarasu@users.noreply.github.com>
2023-05-25 22:16:46 +0300

    Update connect3.php

--- connect3.php
+++ connect3.php
@@ -5,7 +5,7 @@
 	$conn = new mysqli('localhost','root','','tablo');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
-		die("Connection Failed : " . $conn->connect_error);
+		die("Connection Failed:" . $conn->connect_error);
 	}else{
 		$stmt = $conn->prepare("insert into tabloo(Email) values(?)");
 		$stmt->bind_param("s",$email);
